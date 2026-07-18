2ccf8dcf4f73f7a9d8f2773b7caa9caf7abb5c84
edumoranpena-maker <edu.moran.pena@gmail.com>
2026-07-18 17:05:10 -0500

    Tijtik

--- src/components/icons/TikTokIcon.jsx
+++ src/components/icons/TikTokIcon.jsx
@@ -1,1 +1,7 @@
-
+export default function TikTokIcon({ className = 'w-5 h-5' }) {
+  return (
+    <svg viewBox="0 0 24 24" fill="currentColor" className={className} aria-hidden="true">
+      <path d="M16.6 5.82c-.94-.87-1.48-2.1-1.48-3.42h-3.14v13.44c0 1.5-1.22 2.72-2.72 2.72a2.72 2.72 0 0 1 0-5.44c.26 0 .52.04.76.11V9.9a5.86 5.86 0 0 0-.76-.05A5.86 5.86 0 1 0 15.02 15.7V9.24a7.4 7.4 0 0 0 4.33 1.39V7.5a4.4 4.4 0 0 1-2.75-1.68z" />
+    </svg>
+  )
+}
