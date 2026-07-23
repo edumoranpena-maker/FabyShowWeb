04c78e38694a7116123f978cbf30b76900de6c4f
edumoranpena-maker <edu.moran.pena@gmail.com>
2026-07-23 17:31:27 -0500

    Bigger logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,7 +50,7 @@ export default function Navbar() {
             <img
               src="/logo-faby-show.png"
               alt="Faby Show"
-              className="h-8 md:h-10 w-auto"
+              className="h-12 md:h-16 w-auto"
             />
           </a>
 
